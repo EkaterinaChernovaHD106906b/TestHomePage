c59a58027a41647e45a64833eb5b1c9e1e57b64a
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-12-21 23:21:26 +0300

    bug report was added

--- src/test/java/HomePageTesting.java
+++ src/test/java/HomePageTesting.java
@@ -36,14 +36,14 @@ public class HomePageTesting {
     }
 
     @Test
-    void loginWithRegisteredUser() {
+    void loginFromSignUpPage() {
         $(" a[href='/signup']").click();
-        $("input[name='email']").setValue("6hjhjjwr@gmail.com");
-        $("input[name='password']").setValue("k6hjhjwud3");
+        $("input[name='email']").setValue("6hjhJhjjwr@gmail.com");
+        $("input[name='password']").setValue("kO6hjhjwud3");
         $x("//button[text()='Sign Up']").click();
         $("div[class='container has-text-centered']").shouldBe(Condition.visible, Duration.ofSeconds(30));
-        $("input[name='email']").setValue("6hjhjjwr@gmail.com");
-        $("input[name='password']").setValue("k6hjhjwud3");
+        $("input[name='email']").setValue("6hjhJhjjwr@gmail.com");
+        $("input[name='password']").setValue("kO6hjhjwud3");
         $x("//button[text()='Login']").click();
         $x("//h1[contains(text(),'Welcome')]").shouldBe(Condition.visible, Duration.ofSeconds(30));
 
@@ -56,6 +56,38 @@ public class HomePageTesting {
         $x("//button[text()='Sign Up']").click();
         $x("//h3[contains(text(),'Sign Up')]").shouldBe(Condition.visible);
     }
+
+    @Test
+    void registrationWithEmailOnlyNumbers() {
+        $(" a[href='/signup']").click();
+        $("input[name='email']").setValue("525665@gmail.com");
+        $("input[name='password']").setValue(faker.internet().password());
+        $x("//button[text()='Sign Up']").click();
+        $x("//div[contains(text(),'Please check your login')").shouldBe(Condition.visible, Duration.ofSeconds(10));
+
+    }
+
+    @Test
+    void logOut() {
+        $(" a[href='/login']").click();
+        $("input[name='email']").setValue("Hrtgdd@gmail.com");
+        $("input[name='password']").setValue("pass");
+        $x("//button[text()='Login']").click();
+        $(" a[href='/logout']").click();
+        $x("//h1[contains(text(),'Test home page')]").shouldBe(Condition.visible, Duration.ofSeconds(30));
+
+    }
+
+    @Test
+    void loginFromHomePage() {
+        $(" a[href='/login']").click();
+        $("input[name='email']").setValue("Hrtgdd@gmail.com");
+        $("input[name='password']").setValue("pass");
+        $x("//button[text()='Login']").click();
+        $x("//h1[contains(text(),'Welcome')]").shouldBe(Condition.visible, Duration.ofSeconds(30));
+
+    }
+
 
 }
 
