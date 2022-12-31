d080f4897d07bf2583a188b9738f57827900d252
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-12-31 13:59:51 +0300

    Test was modified

--- src/test/java/HomePageTesting.java
+++ src/test/java/HomePageTesting.java
@@ -1,6 +1,10 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +19,15 @@ public class HomePageTesting {
     void setUp() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:5000/login");
+    }
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
@@ -38,8 +51,8 @@ public class HomePageTesting {
     @Test
     void loginFromSignUpPage() {
         $(" a[href='/signup']").click();
-        $("input[name='email']").setValue("6hjhJhjjwr@gmail.com");
-        $("input[name='password']").setValue("kO6hjhjwud3");
+        $("input[name='email']").setValue("H6ORj@gmail.com");
+        $("input[name='password']").setValue("LIG43ud3");
         $x("//button[text()='Sign Up']").click();
         $("div[class='container has-text-centered']").shouldBe(Condition.visible, Duration.ofSeconds(30));
         $("input[name='email']").setValue("6hjhJhjjwr@gmail.com");
@@ -60,28 +73,28 @@ public class HomePageTesting {
     @Test
     void registrationWithEmailOnlyNumbers() {
         $(" a[href='/signup']").click();
-        $("input[name='email']").setValue("525665@gmail.com");
+        $("input[name='email']").setValue("52665@gmail.com");
         $("input[name='password']").setValue(faker.internet().password());
         $x("//button[text()='Sign Up']").click();
-        $x("//div[contains(text(),'Please check your login')").shouldBe(Condition.visible, Duration.ofSeconds(10));
+        $x("//div[contains(text(),'Please check your login')]").shouldBe(Condition.visible, Duration.ofSeconds(10));
 
     }
 
     @Test
     void logOut() {
         $(" a[href='/login']").click();
-        $("input[name='email']").setValue("Hrtgdd@gmail.com");
+        $("input[name='email']").setValue("myEmail@gmail.com");
         $("input[name='password']").setValue("pass");
         $x("//button[text()='Login']").click();
-        $(" a[href='/logout']").click();
+        $(" div.navbar-end>a[href='/logout']").click();
         $x("//h1[contains(text(),'Test home page')]").shouldBe(Condition.visible, Duration.ofSeconds(30));
 
     }
 
     @Test
     void loginFromHomePage() {
-        $(" a[href='/login']").click();
-        $("input[name='email']").setValue("Hrtgdd@gmail.com");
+        $("div> a:nth-of-type(2)").click();
+        $("form div div input[placeholder='Your Email']").setValue("myEmail@gmail.com");
         $("input[name='password']").setValue("pass");
         $x("//button[text()='Login']").click();
         $x("//h1[contains(text(),'Welcome')]").shouldBe(Condition.visible, Duration.ofSeconds(30));
